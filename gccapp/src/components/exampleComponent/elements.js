5b419db046367af525d2e8acdb95f5f6dcd44f2c
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-04-30 11:51:29 +0200

    Add initial files for React app and setup an example component

---
+++ gccapp/src/components/exampleComponent/elements.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Yellow } from "../../color";
+
+export const ExampleComponentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+    width: 100%;
+    background-color: ${Yellow};
+`;
