0d665122c485c74c976f27a736f8c6f46cba6649
TitouanDsch <titouandeschanels@users.noreply.github.com>
2024-05-04 21:43:47 +0200

    Add Global Citizenship Certificate page and routes

--- gccapp/src/App.tsx
+++ gccapp/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/home";
+import Journey from "./pages/journey";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/journey" element={<Journey />} />
       </Routes>
     </Router>
 
