490da0e6ac121919471d679f10271496cab9df4b
Hyeonji Hwang <hj02.hwang@gmail.com>
2024-05-27 12:10:51 +0200

    feat : user journey map store #10

---
+++ gccapp/src/position_store.tsx
@@ -0,0 +1,40 @@
+import create from "zustand";
+import { persist } from "zustand/middleware";
+
+// define the previous state's type
+interface InitialState {
+    currentLevel: number;
+}
+
+// make a store of Zustand
+export const useLevelStore = create(
+    persist<InitialState>(
+        (set) => ({
+            // state
+            currentLevel: 1, // set the basic level as 1
+            // the function of bringing current level
+            getCurrentLevel: () => {
+                return localStorage.getItem("currentLevel")
+                    ? parseInt(localStorage.getItem("currentLevel")!)
+                    : 1;
+            },
+            // the function of setting current level
+            setCurrentLevel: (level: number) => {
+                set({ currentLevel: level });
+                localStorage.setItem("currentLevel", level.toString());
+            },
+            // the function of increasing level whenever students complete modules
+            completeModule: () => {
+                const currentLevel = localStorage.getItem("currentLevel")
+                    ? parseInt(localStorage.getItem("currentLevel")!)
+                    : 1;
+                const newLevel = currentLevel + 1;
+                set({ currentLevel: newLevel });
+                localStorage.setItem("currentLevel", newLevel.toString());
+            },
+        }),
+        { name: "user-Level" }
+    )
+);
+
+//export default useLevelStore;
