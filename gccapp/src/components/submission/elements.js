f6cc4082e50e88e723a00d9adf3a3df411dd7240
Marina Oroz <moroz@hm.edu>
2024-06-03 20:19:02 +0200

    makeover submission point

--- gccapp/src/components/submission/elements.js
+++ gccapp/src/components/submission/elements.js
@@ -1,90 +1,51 @@
 import styled from "styled-components";
-import { White, Blue, Orange, LightGray, Green } from "../../colors";
+import { White, LightGray, Green, Gray } from "../../colors";
 
 export const SubmissionBox = styled.div`
-    margin: 0px 0px;
-    background-color: ${White};
     display: flex;
     flex-direction: column;
-    height: 100vh;
-    overflow-y : scroll;
+    padding-bottom: 50px;
+
 `;
 
-export const Navigation = styled.div`
-    position: relative;
-    margin-left: 222px;
-    padding-top: 80px;
-    font-weight: 150;
-    font-size: 13px;
-    height: 20px;
-    width: 300px;
+export const IntroNavigation = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    align-items: center;
+    margin-left: 40px;
+    color: ${Gray};
+    :last-child {
+        font-weight: bold;
+    }
 `;
 
 export const TitleBox = styled.div`
-    margin: 0px 0px;
-    background-color: ${White};
+    font-weight: bold;
+    font-size: 20px;
     display: flex;
+    max-width: 100%;
     flex-direction: row;
-    height: 10vh;
-`;
-
-export const Title = styled.div`
-    position: relative;
-    margin-left: 220px;
-    padding-top: 0px;
-    font-weight: 600;
-    font-size: 45px;
-    height: 45px;
-    width: 450px;
-`;
-
-export const CompletedCircle = styled.div`
-    position: relative;
-    margin-left: 220px;
-    padding-top: 0px;
-    min-width: 35px;            
-    max-height: 35px;           
-    background-color: ${Green};  
-    border-radius: 50%;      
-`;
-
-export const Completed = styled.div`
-    position: relative;
-    margin-left: 13px;
-    padding-top: 0px;
-    font-weight: 550;
-    font-size: 20px;
-    height: 20px;
-    width: 100px;
-`;
-
-export const OngoingCircle = styled.div`
-    position: relative;
     margin-left: 40px;
-    padding-top: 0px;
-    min-width: 35px;            
-    max-height: 35px;           
-    background-color: ${Orange};  
-    border-radius: 50%;
+    margin-right: 40px;
+    justify-content: space-between;
 `;
 
-export const Ongoing = styled.div`
-    position: relative;
-    margin-left: 13px;
-    padding-top: 0px;
-    font-weight: 550;
-    font-size: 20px;
-    height: 20px;
-    width: 100px;
+export const ExplanationCircle = styled.div`
+    width: 35px;            
+    height: 35px;         
+    border-radius: 50%;     
 `;
 
-export const SyncButton = styled.div`
-    position: relative;
-    margin-left: 35px;
-    margin-top: -12px;
-    padding-top: 0px;
+export const TdWithPadding = styled.td`
+    padding: 0 10px;
+`;
+
+export const SyncButton = styled.button`
     width: 60px;            
     height: 60px;
+    border: none;
+    background-color: ${White};
 
     img {
         max-width: 100%;
@@ -94,120 +55,64 @@ export const SyncButton = styled.div`
 `;
 
 export const SubmissionContainer = styled.div`
-    margin-left: 150px;
-    padding-top: 40px;
-    background-color: ${White};
-    display: flex;
     flex-direction: column;
-    height: 80vh;
+    display: flex;
+    justify-content: center; 
+    align-items: center;
 `;
 
-export const Submission1 = styled.div`
-    margin: 0px 0px;
-    padding-top: 80px;
-    background-color: ${White};
+export const SubmissionItem = styled.div`
+    padding: 20px;
     display: flex;
     flex-direction: row;
     align-items: center;
-    height: 10vh;
+    gap: 50px;
 `;
 
-export const Circle1 = styled.div`
-    position: relative;
-    margin-left: 150px;
-    padding: 0px 0px;
-    min-width: 60px;            
-    min-height: 60px;           
-    background-color: ${Green};  
-    border-radius: 50%;      
-    border: 7px solid ${LightGray};
-`;
-
-export const Description1 = styled.div`
-    position: relative;
-    margin-left: 80px;
-    padding-top: 0px;
-    width: 820px;            
-    height: 110px;           
-    background-color: ${Green};  
-    border-radius: 20px;      
-`;
-
-export const Submission2 = styled.div`
-    margin: 0px 0px;
-    padding-top: 80px;
-    background-color: ${White};
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    height: 10vh;
-`;
-
-export const Circle2 = styled.div`
-    position: relative;
-    margin-left: 150px;
-    padding: 0px 0px;
+export const Circle = styled.div`
     width: 60px;            
     height: 60px;           
-    background-color: ${Orange};  
     border-radius: 50%;      
     border: 7px solid ${LightGray};
 `;
 
-export const Description2 = styled.div`
-    position: relative;
-    margin-left: 80px;
-    padding-top: 0px;
-    width: 820px;            
-    height: 110px;           
-    background-color: ${Orange};  
-    border-radius: 20px;      
-`;
-
-export const Submission3 = styled.div`
-    margin: 0px 0px;
-    padding-top: 80px;
-    background-color: ${White};
+export const Box = styled.div`
     display: flex;
-    flex-direction: row;
     align-items: center;
-    height: 10vh;
+    padding: 5px;
+    width: 50vw;            
+    height: 10vh;           
+    background-color: ${Green};  
+    border-radius: 20px;  
+    
+    table {
+        padding: 10px;
+        width: 100%;
+    }
 `;
 
-export const Circle3 = styled.div`
-    position: relative;
-    margin-left: 150px;
-    padding: 0px 0px;
-    width: 67px;            
-    height: 67px;           
-    background-color: ${LightGray};  
-    border-radius: 50%;      
+export const FileName = styled.p`
+    font-size: 10px;
+    margin-right: 10px;
 `;
 
-export const Description3 = styled.div`
-    position: relative;
-    margin-left: 80px;
-    padding-top: 0px;
-    width: 820px;            
-    height: 110px;           
-    background-color: ${LightGray};  
-    border-radius: 20px;      
+export const UploadContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end; 
 `;
 
-export const TextBox = styled.div`
-    position: relative;
-    padding-left: 40px;
-    padding-top: 40px;
-    font-size: 17px;
-    color: ${White};
-    height: 110px;
-    width: 820px;
-    font-weight: bold
-`;
+export const UploadButton = styled.button`
+    background-color: ${White};
+    height: 50px;
+    width: 50px;
+    border: none;
+    box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+    border-radius: 50%;
 
-export const EmptyBox = styled.div`
-margin: 0px 0px;
-background-color: ${White};
-display: flex;
-height: 10vh;
+    img {
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
+    }
 `;
--- gccapp/src/components/submission/index.tsx
+++ gccapp/src/components/submission/index.tsx
@@ -1,111 +1,144 @@
+import React, { useState } from "react";
 import {
     SubmissionBox,
-    Navigation,
     TitleBox,
-    Title,
-    CompletedCircle,
-    Completed,
-    OngoingCircle,
-    Ongoing,
     SyncButton,
     SubmissionContainer,
-    Submission1, Submission2, Submission3,
-    Circle1, Circle2, Circle3,
-    Description1, Description2, Description3,
-    TextBox, EmptyBox,
+    SubmissionItem,
+    Circle,
+    Box,
+    IntroNavigation,
+    ExplanationCircle,
+    TdWithPadding,
+    UploadButton,
+    FileName,
+    UploadContainer,
 } from "../submission/elements";
-import React from "react";
 import Sync from '../../assets/icons/sync.png';
+import { ReactComponent as NavArrow } from "../../assets/icons/intro-navigate-arrow.svg";
+import { Green, LightGray, Orange } from "../../colors";
+import Upload from '../../assets/icons/upload.png'
 
-interface SubmissionProps{}
+interface FileUploadStatus {
+    files: File[];
+    uploaded: boolean;
+}
+
+interface SubmissionProps { }
+
 const Submissionlayout: React.FC<SubmissionProps> = () => {
+    const [fileUploads, setFileUploads] = useState<FileUploadStatus[]>(Array(6).fill({ files: [], uploaded: false }));
+
+    const handleUpload = (index: number, files: FileList | null) => {
+        if (!files) return;
+
+        const updatedFileUploads = [...fileUploads];
+        updatedFileUploads[index] = { files: Array.from(files), uploaded: true };
+        setFileUploads(updatedFileUploads);
+    };
+
+    const modules = [
+        "This is me",
+        "Me and my circles",
+        "The influence of perceptions",
+        "Dilemmas",
+        "Challende and goal setting",
+        "Value-based challenge creation"
+    ];
+
     return (
         <>
-        <SubmissionBox>
-            <Navigation>
-                Home {'>'} Journey {'>'} Submission Point
-            </Navigation>
+            <SubmissionBox>
+                <IntroNavigation>
+                    <p>Home</p>
+                    <NavArrow />
+                    <p>Journey</p>
+                    <NavArrow />
+                    <p>Submission Point</p>
+                </IntroNavigation>
+                <TitleBox>
+                    <h2>Submission Point</h2>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <TdWithPadding>
+                                    <ExplanationCircle style={{ backgroundColor: Green }} />
+                                </TdWithPadding>
+                                <TdWithPadding>
+                                    <h5>Completed</h5>
+                                </TdWithPadding>
+                                <TdWithPadding>
+                                    <ExplanationCircle style={{ backgroundColor: Orange }} />
+                                </TdWithPadding>
+                                <TdWithPadding>
+                                    <h5>Ongoing</h5>
+                                </TdWithPadding>
+                                <TdWithPadding>
+                                    <SyncButton>
+                                        <img src={Sync} alt="Sync Button" />
+                                    </SyncButton>
+                                </TdWithPadding>
+                            </tr>
+                        </tbody>
+                    </table>
+                </TitleBox>
+                <SubmissionContainer>
+                    {fileUploads.map((fileUpload, index) => (
+                        <SubmissionItem key={index}>
+                            <Circle style={{
+                                backgroundColor:
+                                    // If it's the first module and no file has been uploaded or the previous module is uploaded and the current one isn't -> orange
+                                    (index === 0 && !fileUpload.uploaded) || (fileUploads[index - 1]?.uploaded && !fileUpload.uploaded) ? Orange :
+                                        // If the current module is uploaded -> green; otherwise -> gray
+                                        (fileUpload.uploaded ? Green : LightGray)
+                            }} />
+                            <Box style={{
+                                backgroundColor:
+                                    // If it's the first module and no file has been uploaded or the previous module is uploaded and the current one isn't -> orange
+                                    (index === 0 && !fileUpload.uploaded) || (fileUploads[index - 1]?.uploaded && !fileUpload.uploaded) ? Orange :
+                                        // If the current module is uploaded -> green; otherwise -> gray
+                                        (fileUpload.uploaded ? Green : LightGray)
+                            }} >
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <h3>{modules[index]}</h3>
+                                            </td>
+                                            <td>
+                                                <UploadContainer>
+                                                    <div>
+                                                        {fileUpload.files.map((file, i) => (
+                                                            <div key={i}>
+                                                                <FileName>{file.name}</FileName>
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                    <UploadButton>
+                                                        <label htmlFor={`file-upload-${index}`} className="custom-file-upload">
+                                                            <img src={Upload} alt="upload" />
+                                                        </label>
+                                                        <input
+                                                            id={`file-upload-${index}`}
+                                                            type="file"
+                                                            multiple
+                                                            onChange={(e) => handleUpload(index, e.target.files)}
+                                                            style={{ display: "none" }}
+                                                        />
+                                                    </UploadButton>
+                                                </UploadContainer>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </Box>
+                        </SubmissionItem>
+                    ))}
+                </SubmissionContainer>
 
-            <TitleBox>
-                <Title>
-                    Submission Point
-                </Title>
-                <CompletedCircle>
-                    </CompletedCircle>
-                <Completed>
-                    Completed
-                </Completed>
-                <OngoingCircle>
-                    </OngoingCircle>
-                <Ongoing>
-                    On going
-                </Ongoing>
-                <SyncButton>
-                    <img src={Sync} alt="Sync Button" />
-                </SyncButton>
-            </TitleBox>
-
-            <SubmissionContainer>
-                <Submission1>
-                    <Circle1>
-                    </Circle1>
-                    <Description1>
-                        <TextBox>
-                            Any description
-                        </TextBox>
-                    </Description1>
-                </Submission1>
-                <Submission1>
-                    <Circle1>
-                    </Circle1>
-                    <Description1>
-                    </Description1>
-                </Submission1>
-                <Submission1>
-                    <Circle1>
-                    </Circle1>
-                    <Description1>
-                    </Description1>
-                </Submission1>
-
-                <Submission2>
-                    <Circle2>
-                    </Circle2>
-                    <Description2>
-                    </Description2>
-                </Submission2>
-
-                <Submission3>
-                    <Circle3>
-                    </Circle3>
-                    <Description3>
-                    </Description3>
-                </Submission3>
-                <Submission3>
-                    <Circle3>
-                    </Circle3>
-                    <Description3>
-                    </Description3>
-                </Submission3>
-                <Submission3>
-                    <Circle3>
-                    </Circle3>
-                    <Description3>
-                    </Description3>
-                </Submission3>
-                <Submission3>
-                    <Circle3>
-                    </Circle3>
-                    <Description3>
-                    </Description3>
-                </Submission3>
-            </SubmissionContainer>
-        </SubmissionBox>
-
-        <EmptyBox>
-        </EmptyBox>
+            </SubmissionBox>
         </>
-    )
-}
+    );
+};
 
 export default Submissionlayout;
